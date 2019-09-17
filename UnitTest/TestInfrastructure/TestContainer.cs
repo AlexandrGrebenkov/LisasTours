f90732bffa21a487d19159424375ff2ec75710ed
Alexandr Grebenkov <alexandrgrebenkov@gmail.com>
2019-09-17 22:10:11 +0500

    Test infrastructure

---
+++ UnitTest/TestInfrastructure/TestContainer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Autofac;
+using Moq;
+
+namespace UnitTests.TestInfrastructure
+{
+    public class TestContainer : IDisposable
+    {
+        public IContainer Container { get; private set; }
+
+        public TestContainer() : this(UnitTestBindings.CreateContainer(autoMock: true))
+        {
+        }
+
+        protected TestContainer(IContainer container)
+        {
+            Container = container;
+        }
+
+        protected T Get<T>()
+        {
+            return Container.Resolve<T>();
+        }
+
+        protected Mock<T> GetMock<T>() where T : class
+        {
+            bool registerMock = false;
+            if (Container.IsRegistered(typeof(T)))
+            {
+                try
+                {
+                    Mock.Get(Container.Resolve<T>());
+                }
+                catch
+                {
+                    registerMock = true;
+                }
+            }
+
+            if (registerMock || !Container.IsRegistered(typeof(T)))
+            {
+                var mockInstance = (new Mock<T>() { CallBase = true }).Object;
+
+                var updater = new ContainerBuilder();
+                updater.RegisterInstance(mockInstance).As<T>();
+#pragma warning disable CS0618 // Type or member is obsolete
+                updater.Update(Container);
+#pragma warning restore CS0618 // Type or member is obsolete
+            }
+
+            T instance = Container.Resolve<T>();
+            return Mock.Get(instance);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (Container != null)
+            {
+                Container.Dispose();
+                Container = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+    }
+}
---
+++ UnitTest/TestInfrastructure/UnitTestBindings.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Autofac;
+using AutoMapper;
+using LisasTours;
+using LisasTours.Application.Queries;
+using LisasTours.Controllers;
+using LisasTours.Services;
+using MediatR;
+using Moq;
+
+namespace UnitTests.TestInfrastructure
+{
+    internal class UnitTestBindings : Module
+    {
+        internal static IContainer CreateContainer(bool autoMock)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<UnitTestBindings>();
+
+            if (autoMock)
+            {
+
+            }
+
+            var container = builder.Build();
+            return container;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<CompaniesController>();
+
+            builder.Register(_ => new Mock<IExporter>().Object).SingleInstance();
+            builder.Register(_ => new Mock<ICompanyQueries>().Object).SingleInstance();
+            builder.Register(_ => new Mock<IMediator>().Object).SingleInstance();
+
+            builder.RegisterAssemblyTypes(typeof(Startup).Assembly).As<Profile>();
+
+            builder.Register(context => new MapperConfiguration(cfg =>
+            {
+                foreach (var profile in context.Resolve<IEnumerable<Profile>>())
+                {
+                    cfg.AddProfile(profile);
+                }
+            })).AsSelf().SingleInstance();
+            builder.Register(c => c.Resolve<MapperConfiguration>().CreateMapper(c.Resolve))
+                .As<IMapper>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
